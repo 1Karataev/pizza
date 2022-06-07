7c6c0a67399e0185603b03682e95c32df77aef2d
1Karataev <karataev00@inbox.ru>
2022-06-07 22:02:36 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useMemo, useState } from 'react';
+import ContentTop from './components/ContentTop';
+import Header from './components/Header';
+import PizzaBlock from './components/PizzaBlock';
+import './scss/app.scss'
+import Pizza from '../src/assets/img/pizza.json'
+
+function App() {
+
+  return (
+    
+      <div className="wrapper">
+      <Header/>
+      <div className="content">
+        <div className="container">
+          <ContentTop/>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {
+              Pizza.map((pizza,i ) => <PizzaBlock key={i} {...pizza}/>)
+            }
+            
+            
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    
+      
+  );
+}
+
+export default App;
